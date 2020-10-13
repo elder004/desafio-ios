727ff8511f3d58296a1f960fb35c31b0b5ed3b98
Elder Santos <elder004@gmail.com>
2020-10-13 15:10:16 -0300

    Criação do projeto e instalação das libs principais

---
+++ SwiftReceipts/Pods/Target Support Files/Pods-SwiftReceipts-SwiftReceiptsUITests/Pods-SwiftReceipts-SwiftReceiptsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftReceipts_SwiftReceiptsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftReceipts_SwiftReceiptsUITests
+@end
---
+++ SwiftReceipts/Pods/Target Support Files/Pods-SwiftReceipts/Pods-SwiftReceipts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftReceipts : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftReceipts
+@end
